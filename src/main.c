4e3063ff081832f689a313857adec63d350bc332
ennkp <nkr9299@gmail.com>
2023-09-22 21:21:42 +0530

    implement all instructions except for the ones that use input and timers

--- src/main.c
+++ src/main.c
@@ -7,8 +7,19 @@
         exit(1);                        \
     } while (0)
 
+// use
+// #define DEBUG_LOG
+// for printing logs to stderr.
+// Pipe the output of stderr to a log like to not break the display
+// Eg: chip8 <rom> 2> log
+
 #ifdef DEBUG_LOG
-#define DEBUG(...)              printf(__VA_ARGS__)
+#define DEBUG(...)                      \
+    do {                                \
+        fprintf(stderr, __VA_ARGS__);   \
+        fprintf(stderr, "\n");          \
+    } while (0)
+
 #else
 #define DEBUG(...)
 #endif
@@ -32,11 +43,11 @@
 #define X(instruction)          ((instruction & 0x0F00) >> 8)
 #define Y(instruction)          ((instruction & 0x00F0) >> 4)
 #define N(instruction)          (instruction & 0x000F)
+#define NN(instruction)         (instruction & 0x00FF)
 #define NNN(instruction)        (instruction & 0x0FFF)
 
 // cycle constants
-#define TARGET_FRAME_RATE       60
-#define FRAME_TIME              (1000/TARGET_FRAME_RATE)
+#define DEFAULT_CYCLES_PER_SEC  60
 
 #define KEYCODE_ESC             27
 
@@ -61,9 +72,10 @@ static uint8_t FONT_DATA[] = {
 
 typedef struct {
     uint8_t    mem[MEM_SIZE];
-    uint32_t   pc;
+    uint16_t   pc;
     uint16_t   i;
     uint16_t   stack[STACK_SIZE];
+    uint8_t    sp;
     uint8_t    delay_timer;
     uint8_t    sound_timer;
     uint8_t    v[REG_COUNT];
@@ -111,13 +123,13 @@ void chip8_load_rom(Chip8 *c, const char *file_path) {
 static inline
 uint16_t chip8_fetch(Chip8 *c) {
     if (c->pc + 2 > MEM_SIZE) {
-        DEBUG("Reached end of memory: %u\n", c->pc);
+        DEBUG("Reached end of memory: %u", c->pc);
         exit(1);
     }
 
     uint16_t instruction = c->mem[c->pc] << BYTE_SIZE | c->mem[c->pc+1];
     c->pc += 2;
-    DEBUG("instruction: %x\n", instruction);
+    DEBUG("instruction: %x", instruction);
     return instruction;
 }
 
@@ -170,56 +182,6 @@ void chip8_load_pixels(Chip8 *c, uint8_t x, uint8_t y, uint8_t h) {
 }
 
 static inline
-void chip8_decode_execute(Chip8 *c, uint16_t instruction) {
-    switch (instruction >> 12) {
-        case 0x0: {
-                      if (instruction == 0x0E00) {
-                          DEBUG("Clear screen\n");
-                          chip8_clear_screen(c);
-                      }
-                      break;
-                  }
-        case 0x1: {
-                      const uint32_t jmp_pos = NNN(instruction);
-                      DEBUG("Jump to %u\n", jmp_pos);
-                      c->pc = jmp_pos;
-                      break;
-                  }
-        case 0x6: {
-                      const uint8_t reg = X(instruction);
-                      const uint8_t val = N(instruction);
-                      c->v[reg] = val;
-                      DEBUG("v[%u] = %u\n", reg, val);
-                      break;
-                  }
-        case 0x7: {
-                      const uint8_t reg = X(instruction);
-                      const uint8_t val = N(instruction);
-                      c->v[reg] += val;
-                      DEBUG("v[%u] += %u\n", reg, val);
-                      break;
-                  }
-        case 0xA: {
-                      const uint16_t val = NNN(instruction);
-                      c->i = val;
-                      DEBUG("i = %u\n", val);
-                      break;
-                  }
-        case 0xD: {
-                      const uint8_t x = c->v[X(instruction)];
-                      const uint8_t y = c->v[Y(instruction)];
-                      const uint8_t h = N(instruction);
-                      DEBUG("draw %u, %u, %u\n", x, y, h);
-                      chip8_load_pixels(c, x, y, h);
-                      break;
-                  }
-        default:  {
-                      DEBUG("Unrecognized instruction: %u\n", instruction);
-                  }
-    }
-}
-
-static inline
 void chip8_display(Chip8 *c) {
     for (int y = 0; y < DISPLAY_HEIGHT; ++y) {
         for (int x = 0; x < DISPLAY_WIDTH; ++x) {
@@ -227,6 +189,223 @@ void chip8_display(Chip8 *c) {
             print_as_formatted_binary(byte);
         }
         printf(SET_DEFAULT"\n");
+    }
+    platform_cursor_up(DISPLAY_HEIGHT);
+}
+
+static inline
+void chip8_decode_execute(Chip8 *c, uint16_t instruction) {
+    switch (instruction >> 12) {
+        case 0x0: {
+                      switch (instruction) {
+                      case 0x0E00:
+                          DEBUG("Clear screen");
+                          chip8_clear_screen(c);
+                          break;
+                      case 0x00EE:
+                          {
+                              const uint16_t jmp_pos = c->stack[--c->sp];
+                              DEBUG("Return to %u", jmp_pos);
+                              c->pc = jmp_pos;
+                              break;
+                          }
+                      }
+                      break;
+                  }
+        case 0x1: {
+                      const uint16_t jmp_pos = NNN(instruction);
+                      DEBUG("Jump to %u", jmp_pos);
+                      c->pc = jmp_pos;
+                      break;
+                  }
+        case 0x2: {
+                      const uint16_t jmp_pos = NNN(instruction);
+                      DEBUG("Push to stack: %u -> call %u", c->pc, jmp_pos);
+                      c->stack[c->sp++] = c->pc;
+                      c->pc = jmp_pos;
+                      break;
+                  }
+        case 0x3: {
+                      const uint8_t reg = X(instruction);
+                      const uint8_t val = NN(instruction);
+                      DEBUG("Skip if v%u (%u) == %u", reg, c->v[reg], val);
+                      c->pc += (c->v[reg] == val) * 2;
+                      break;
+                  }
+        case 0x4: {
+                      const uint8_t reg = X(instruction);
+                      const uint8_t val = NN(instruction);
+                      DEBUG("Skip if v%u (%u) != %u", reg, c->v[reg], val);
+                      c->pc += (c->v[reg] != val) * 2;
+                      break;
+                  }
+        case 0x5: {
+                      const uint8_t x = X(instruction);
+                      const uint8_t y = Y(instruction);
+                      DEBUG("Skip if v%u (%u) == v%u (%u)", x, c->v[x], y, c->v[y]);
+                      c->pc += (c->v[x] == c->v[y]) * 2;
+                      break;
+                  }
+        case 0x9: {
+                      const uint8_t x = X(instruction);
+                      const uint8_t y = Y(instruction);
+                      DEBUG("Skip if v%u (%u) != v%u (%u)", x, c->v[x], y, c->v[y]);
+                      c->pc += (c->v[x] != c->v[y]) * 2;
+                      break;
+                  }
+        case 0x6: {
+                      const uint8_t reg = X(instruction);
+                      const uint16_t val = NN(instruction);
+                      c->v[reg] = val;
+                      DEBUG("v[%u] = %u", reg, val);
+                      break;
+                  }
+        case 0x7: {
+                      const uint8_t reg = X(instruction);
+                      const uint16_t val = NN(instruction);
+                      c->v[reg] += val;
+                      DEBUG("v[%u] += %u", reg, val);
+                      break;
+                  }
+        case 0x8: {
+                      const uint8_t x = X(instruction);
+                      const uint8_t y = Y(instruction);
+                      switch (N(instruction)) {
+                      case 0x0:
+                          c->v[x] = c->v[y];
+                          DEBUG("v%u = v%u (%x)", x, y, c->v[y]);
+                          break;
+                      case 0x1:
+                          c->v[x] |= c->v[y];
+                          DEBUG("v%u |= v%u (%x) => %x", x, y, c->v[y], c->v[x]);
+                          break;
+                      case 0x2:
+                          c->v[x] &= c->v[y];
+                          DEBUG("v%u &= v%u (%x) => %x", x, y, c->v[y], c->v[x]);
+                          break;
+                      case 0x3:
+                          c->v[x] ^= c->v[y];
+                          DEBUG("v%u ^= v%u (%x) => %x", x, y, c->v[y], c->v[x]);
+                          break;
+                      case 0x4:
+                          {
+                              // TODO: optimize this?
+                              const uint16_t sum = (c->v[x] + c->v[y]);
+                              c->v[x] = sum;
+                              c->v[0xF] = sum > 0xFF;
+                              DEBUG("v%u += v%u (%x) => %x", x, y, c->v[y], c->v[x]);
+                              break;
+                          }
+                      case 0x5:
+                          {
+                              const uint8_t vf = c->v[x] >= c->v[y];
+                              c->v[x] -= c->v[y];
+                              c->v[0xF] = vf;
+                              DEBUG("v%u -= v%u (%x) => %x", x, y, c->v[y], c->v[x]);
+                              break;
+                          }
+                      case 0x7:
+                          {
+                              const uint8_t vf = c->v[x] <= c->v[y];
+                              c->v[x] = c->v[y] - c->v[x];
+                              c->v[0xF] = vf;
+                              DEBUG("v%u = v%u - v%u (%x) => %x", x, y, x, c->v[y], c->v[x]);
+                              break;
+                          }
+                      // TODO: add flag to allow setting VX to the value of VY
+                      case 0x6:
+                          {
+                              const uint8_t vf = c->v[x] & (1 << 0) ? 1 : 0;
+                              c->v[x] >>= 1;
+                              c->v[0xF] = vf;
+                              DEBUG("v%u >>= 1 => %u, vf: %u", x, c->v[x], c->v[0xF]);
+                              break;
+                          }
+                      case 0xE:
+                          {
+                              const uint8_t vf = c->v[x] & (1 << 7) ? 1 : 0;
+                              c->v[x] <<= 1;
+                              c->v[0xF] = vf;
+                              DEBUG("v%u <<= 1 => %u, vf: %u", x, c->v[x], c->v[0xF]);
+                              break;
+                          }
+                      }
+                      break;
+                  }
+        case 0xA: {
+                      const uint16_t val = NNN(instruction);
+                      c->i = val;
+                      DEBUG("i = %u", c->i);
+                      break;
+                  }
+        case 0xB: {
+                      // TODO: add flag for using BXNN optionally
+                      const uint16_t val = NNN(instruction);
+                      c->i = val + c->v[0];
+                      DEBUG("i = %u", c->i);
+                      break;
+                  }
+        case 0xC: {
+                      const uint8_t reg = X(instruction);
+                      const uint16_t val = NN(instruction);
+                      const uint8_t r = rand();
+                      c->v[reg] = r & val;
+                      DEBUG("Rand v%u = %u & %u", reg, r, val);
+                      break;
+                  }
+        case 0xD: {
+                      const uint8_t x = c->v[X(instruction)];
+                      const uint8_t y = c->v[Y(instruction)];
+                      const uint8_t h = N(instruction);
+                      DEBUG("draw %u, %u, %u", x, y, h);
+                      chip8_load_pixels(c, x, y, h);
+                      chip8_display(c);
+                      break;
+                  }
+        case 0xF: {
+                      const uint8_t reg = X(instruction);
+                      switch (NN(instruction)) {
+                      // TODO: delay_timer and sound_timer instructions
+                      case 0x1E:
+                          c->v[0xF] =
+                              (c->i += c->v[reg]) > MEM_SIZE;
+                          DEBUG("i += v%u: %u, vf: %u", reg, c->v[reg], c->v[0xF]);
+                          break;
+                      case 0x29:
+                          c->i = N(c->v[reg]) * 5 + FONT_DATA_OFFSET;
+                          DEBUG("i = %x", N(c->v[reg]));
+                          break;
+                      case 0x33:
+                          {
+                              uint8_t d = c->v[reg];
+                              const uint8_t d3 = d % 10;
+                              const uint8_t d2 = (d /= 10) % 10;
+                              const uint8_t d1 = (d /= 10);
+                              c->mem[c->i] = d1;
+                              c->mem[c->i + 1] = d2;
+                              c->mem[c->i + 2] = d3;
+                              DEBUG("d: %u -> (%u, %u, %u)", c->v[reg], d1, d2, d3);
+                              break;
+                          }
+                      // TODO: add flag for incrementing c->i for supporting older games
+                      case 0x55:
+                          for (int i = 0; i <= reg; ++i) {
+                              c->mem[c->i + i] = c->v[i];
+                              DEBUG("Storing v%u (%u) at mem[%u]", i, c->v[i], i);
+                          }
+                          break;
+                      case 0x65:
+                          for (int i = 0; i <= reg; ++i) {
+                              c->v[i] = c->mem[c->i + i];
+                              DEBUG("Loading v%u from mem[%u] (%u)", i, i, c->mem[i]);
+                          }
+                          break;
+                      }
+                      break;
+                  }
+        default:  {
+                      DEBUG("Unrecognized instruction: %u", instruction);
+                  }
     }
 }
 
@@ -240,6 +419,9 @@ int main(int argc, const char **argv) {
     chip8_load_to_mem(c, FONT_DATA_OFFSET, FONT_DATA, sizeof(FONT_DATA));
     chip8_load_rom(c, argv[1]);
 
+    // TODO: introduce flag for changing cycles_per_sec
+    const uint32_t cycles_per_sec = DEFAULT_CYCLES_PER_SEC;
+
     if (!platform_setup())
         FATAL("Failed to setup platform");
 
@@ -248,15 +430,12 @@ int main(int argc, const char **argv) {
         uint16_t instruction = chip8_fetch(c);
         chip8_decode_execute(c, instruction);
         
-        chip8_display(c);
-
-        platform_cursor_up(DISPLAY_HEIGHT);
-        platform_sleep(FRAME_TIME);
-
         switch (platform_get_keycode()) {
-        case KEYCODE_ESC:
-            goto quit;
+            case KEYCODE_ESC:
+                goto quit;
         }
+
+        platform_sleep(1000/cycles_per_sec);
     }
 
 quit:
